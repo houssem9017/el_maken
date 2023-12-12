9eda89047b7f685b80ea226b8e003eb770b40edb
houssem9017 <houssem.darragi@esprit.tn>
2023-12-12 18:47:38 +0100

    removed db

--- elmaken/settings.py
+++ elmaken/settings.py
@@ -81,10 +81,6 @@ WSGI_APPLICATION = 'elmaken.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
